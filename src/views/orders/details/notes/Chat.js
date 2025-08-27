253c8bd423811d970741aedcf40eca5a4f4d6593
kavindadilshan <kavindadilshan97@gmail.com>
2025-08-27 11:45:29 -0700

    added special note opt

---
+++ src/views/orders/details/notes/Chat.js
@@ -0,0 +1,191 @@
+// ** React Imports
+import ReactDOM from 'react-dom'
+import React, {useState, useEffect, useRef} from 'react'
+
+// ** Custom Components
+import Avatar from '@components/avatar'
+
+// ** Store & Actions
+import {sendMsg} from './store'
+import {useDispatch} from 'react-redux'
+
+// ** Third Party Components
+import classnames from 'classnames'
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import {MessageSquare, Menu, PhoneCall, Video, Search, MoreVertical, Mic, Image, Send} from 'react-feather'
+import {useParams} from "react-router-dom";
+
+// ** Reactstrap Imports
+import {
+    Form,
+    Input,
+    Button,
+    InputGroup
+} from 'reactstrap'
+
+const ChatLog = props => {
+    // ** Props & Store
+    const {handleUser, handleUserSidebarRight, handleSidebar, chatDetails, userSidebarLeft, replyCallback} = props
+
+    // ** Refs & Dispatch
+    const chatArea = useRef(null);
+    const dispatch = useDispatch();
+
+    // ** State
+    const [msg, setMsg] = useState('')
+
+    // ** Scroll to chat bottom
+    const scrollToBottom = () => {
+        const chatContainer = ReactDOM.findDOMNode(chatArea.current)
+        chatContainer.scrollTop = Number.MAX_SAFE_INTEGER
+    }
+
+    // ** If user chat is not empty scrollToBottom
+    useEffect(() => {
+        const chatDetailsLen = Object.keys(chatDetails).length
+        console.log(chatDetails)
+        if (chatDetailsLen) {
+            scrollToBottom()
+        }
+    }, [chatDetails])
+
+    // ** Formats chat data based on sender and groups by date
+    const formattedChatData = () => {
+        let chatLog = []
+        if (chatDetails.chat) {
+            chatLog = chatDetails.chat.chat
+        }
+
+        const formattedChatLog = []
+        let currentDate = null
+        let dateGroup = {
+            date: null,
+            messages: []
+        }
+
+        chatLog.forEach((msg, index) => {
+            const messageDate = new Date(msg.time).toDateString()
+            if (currentDate === messageDate) {
+                dateGroup.messages.push({
+                    senderId: msg.senderId,
+                    msg: msg.message,
+                    time: msg.time
+                })
+            } else {
+                if (dateGroup.messages.length) formattedChatLog.push(dateGroup)
+                currentDate = messageDate
+                dateGroup = {
+                    date: messageDate,
+                    messages: [
+                        {
+                            senderId: msg.senderId,
+                            msg: msg.message,
+                            time: msg.time
+                        }
+                    ]
+                }
+            }
+            if (index === chatLog.length - 1) formattedChatLog.push(dateGroup)
+        })
+        return formattedChatLog
+    }
+
+    // ** Renders user chat with date grouping
+    const renderChats = () => {
+        return formattedChatData().map((group, groupIndex) => (
+            <div key={groupIndex}>
+                <div className='chat-date'>
+                    <h6 className='text-black-50 text-center font-small-3'>{new Date(group.date).toLocaleDateString()}</h6>
+                </div>
+                {group.messages.map((item, index) => (
+                    <div
+                        key={index}
+                        className={classnames('chat', {
+                            'chat-left': item.senderId !== 11
+                        })}
+                    >
+                        <div className='chat-avatar'>
+                            <Avatar
+                                imgWidth={36}
+                                imgHeight={36}
+                                className='box-shadow-1 cursor-pointer'
+                                img={"https://thumbs.dreamstime.com/b/braka-avatar-fotografii-placeholder-profilowa-ikona-124557887.jpg"}
+                            />
+                        </div>
+
+                        <div className='chat-body'>
+                            <div className='chat-content'>
+                                <p>{item.msg}</p>
+                                <small className={`${item.senderId !== 11 ? 'text-muted' : 'text-white'}`}>
+                                    {new Date(item.time).toLocaleTimeString()}
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        ))
+    }
+
+    // ** On mobile screen open left sidebar on Start Conversation Click
+    const handleStartConversation = () => {
+        if (!Object.keys(chatDetails).length && !userSidebarLeft && window.innerWidth < 992) {
+            handleSidebar()
+        }
+    }
+
+    // ** Sends New Msg
+    const handleSendMsg = e => {
+        e.preventDefault()
+        if (msg.length) {
+            // dispatch(sendMsg({...chatDetails, message: msg}))
+            console.log('msg', msg)
+            replyCallback(msg)
+            setMsg('')
+        }
+    }
+
+    // ** ChatWrapper tag based on chat's length
+    const ChatWrapper = Object.keys(chatDetails).length && chatDetails.chat ? PerfectScrollbar : 'div'
+
+    return (
+        <div className='chat-app-window'>
+            <div className={classnames('start-chat-area', {'d-none': Object.keys(chatDetails).length})}>
+                <div className='start-chat-icon mb-1'>
+                    <MessageSquare/>
+                </div>
+                <h4 className='sidebar-toggle start-chat-text' onClick={handleStartConversation}>
+                    Start Conversation
+                </h4>
+            </div>
+            {Object.keys(chatDetails).length ? (
+                <div className={classnames('active-chat', {'d-none': chatDetails === null})}>
+                    <ChatWrapper ref={chatArea} className='user-chats' style={{height: '85%'}}
+                                 options={{wheelPropagation: false}}>
+                        {chatDetails.chat ? <div className='chats'>{renderChats()}</div> : null}
+                    </ChatWrapper>
+
+                    <Form className='chat-app-form' onSubmit={e => handleSendMsg(e)}>
+                        <InputGroup className='input-group-merge me-1 form-send-message'>
+                            <Input
+                                value={msg}
+                                onChange={e => setMsg(e.target.value)}
+                                placeholder='Type your message or use speech to text'
+                                disabled={props.ticketDetails.status !== "open"}
+                            />
+                        </InputGroup>
+                        <Button className='send' color='primary'
+                                disabled={props.ticketDetails.status !== "open"}
+                        >
+                            <Send size={14} className='d-lg-none'/>
+                            <span className='d-none d-lg-block'>Send</span>
+                        </Button>
+                    </Form>
+                </div>
+            ) : null}
+        </div>
+    )
+}
+
+export default ChatLog
+
---
+++ src/views/orders/details/notes/index.js
@@ -0,0 +1,319 @@
+// ** React Imports
+import React, {Fragment, useState, useEffect} from 'react'
+
+// ** Chat App Component Imports
+import Chat from './Chat'
+import Sidebar from './SidebarLeft'
+
+// ** Third Party Components
+import classnames from 'classnames'
+
+// ** Store & Actions
+import {useDispatch, useSelector} from 'react-redux'
+import {getUserProfile, getChatContacts} from './store'
+
+import '@styles/base/pages/app-chat.scss'
+import '@styles/base/pages/app-chat-list.scss'
+
+import {useLocation, useParams} from "react-router-dom";
+import * as TicketServices from "../../../services/tickets";
+import {toggleLoading} from "../../../redux/loading";
+import {notifyMessage, setCurrencyWithSymbol, tableDataDateTimeConverter} from "../../../utility/commonFun";
+import {getTicketChat} from "../../../services/tickets";
+import * as OrderResourcesServices from "../../../services/order-resources";
+import logo from "../../../assets/images/logo/logo.png";
+import {Badge, Button, Col, Row} from "reactstrap";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import {customSweetAlert} from "../../../utility/Utils";
+
+const AppChat = () => {
+    // ** Store Vars
+    const dispatch = useDispatch();
+    const {name} = useParams();
+
+
+    // ** States
+    const [user, setUser] = useState({})
+    const [sidebar, setSidebar] = useState(false)
+    const [userSidebarRight, setUserSidebarRight] = useState(false)
+    const [userSidebarLeft, setUserSidebarLeft] = useState(false)
+    const [chatList, setChatList] = useState({});
+    const [ticketDetails, setTicketDetails] = useState({});
+    const [productItems, setProductItems] = useState([]);
+
+    // ** Sidebar & overlay toggle functions
+    const handleSidebar = () => setSidebar(!sidebar)
+    const handleUserSidebarLeft = () => setUserSidebarLeft(!userSidebarLeft)
+    const handleUserSidebarRight = () => setUserSidebarRight(!userSidebarRight)
+    const handleOverlayClick = () => {
+        setSidebar(false)
+        setUserSidebarRight(false)
+        setUserSidebarLeft(false)
+    }
+
+    // ** Set user function for Right Sidebar
+    const handleUser = obj => setUser(obj)
+
+    // ** Get data on Mount
+    useEffect(() => {
+        dispatch(getChatContacts())
+        dispatch(getUserProfile())
+    }, [dispatch])
+
+    useEffect(() => {
+        getAllChatList();
+    }, [])
+
+    const getAllChatList = () => {
+        dispatch(toggleLoading())
+
+        const body = {
+            "all": 1,
+            "ticket_number": name
+        }
+        TicketServices.getTicketChat(body)
+            .then((res) => {
+                if (res.success) {
+                    dispatch(toggleLoading())
+                    setTicketDetails(res.data[0]);
+                    getOrderDetails(res.data[0].order.id);
+
+                    const list = [];
+                    if (res.data[0]) {
+                        res.data[0].comments.map((item) => {
+                            if (item.user.user_level_id === 2) {
+                                list.push({senderId: 2, message: item.message, time: item.created_at});
+                            } else {
+                                list.push({senderId: 11, message: item.message, time: item.created_at});
+                            }
+                        })
+                    }
+
+                    const chatObj = {
+                        "chat": {
+                            "id": 1,
+                            chat: list
+                        }
+                    }
+
+                    setChatList(chatObj);
+
+
+                } else {
+                    dispatch(toggleLoading())
+                    notifyMessage(res.message, res.status);
+                }
+            })
+    }
+
+    const getOrderDetails = async (id) => {
+        dispatch(toggleLoading());
+        OrderResourcesServices.getOrderByOrderId(id)
+            .then((res) => {
+                if (res.success) {
+                    console.log(res.data);
+                    setProductItems(res.data.order.order_items);
+                    dispatch(toggleLoading());
+                } else {
+                    dispatch(toggleLoading());
+                    notifyMessage(res.message, res.status);
+                }
+            })
+    }
+
+
+    const replyHandler = (msg) => {
+        dispatch(toggleLoading())
+        const body = {
+            "id": ticketDetails?.id,
+            "message": msg
+        }
+        TicketServices.createTicketComment(body)
+            .then((res) => {
+                if (res.success) {
+                    dispatch(toggleLoading())
+
+                    setChatList({
+                        "chat": {
+                            "id": 1,
+                            chat: [...chatList.chat.chat, {senderId: 11, message: msg, time: new Date()}]
+                        }
+                    })
+
+                } else {
+                    dispatch(toggleLoading())
+                    notifyMessage(res.message, res.status);
+                }
+            })
+    }
+
+    const changeStatusOnTicket =async () => {
+
+        await customSweetAlert(
+            'Are you sure you did resolved this ticket?',
+            0,
+            () => {
+                changeStatusOffTicket()
+            }
+        )
+    }
+
+    const changeStatusOffTicket = () => {
+        dispatch(toggleLoading())
+        const body = {
+            "id": ticketDetails?.id,
+            "status": "resolved"
+        }
+
+        TicketServices.changeTicketStatus(body)
+            .then((res) => {
+                console.log(res)
+                if (res.success) {
+                    dispatch(toggleLoading())
+                    notifyMessage(res.message, 1);
+                    getAllChatList();
+                } else {
+                    dispatch(toggleLoading())
+                    notifyMessage(res.message, res.status);
+                }
+            })
+    }
+
+    return (
+        <Fragment>
+            <Sidebar
+                ticketDetails={ticketDetails}
+                changeStatusHandler={() => changeStatusOnTicket()}
+                productItems={productItems}
+            />
+
+            <div className='content-right'>
+
+                <PerfectScrollbar className='chat-user-list-wrapper list-group'>
+                    <div
+                        className='d-flex p-1 justify-content-between flex-md-row flex-column invoice-spacing mt-0 d-lg-none'>
+                        <div className='w-100'>
+                            <div className='logo-wrapper d-flex d-inline mb-1 align-items-center'>
+                                <img src={logo} width={50} height={50} alt={'logo'}/>
+                                <h3 className='text-primary invoice-logo ms-1 mt-25'>CheapHub</h3>
+                            </div>
+                            <Row className="align-items-center">
+                                <Col lg={12}>
+                                    <p className='card-text mb-25'><span
+                                        className="fw-bold text-black">Ticket Number: </span>{ticketDetails?.ticket_number}
+                                    </p>
+                                    <p className='card-text mb-25'><span
+                                        className="fw-bold text-black">Order Number: </span>{ticketDetails?.order?.order_id}
+                                    </p>
+                                    <p className='card-text mb-25'><span
+                                        className="fw-bold text-black">Customer Name: </span> {ticketDetails?.customer?.display_name}
+                                    </p>
+                                    <p className='card-text mb-25'><span
+                                        className="fw-bold text-black">Email: </span> {ticketDetails?.customer?.email}
+                                    </p>
+                                    <p className='card-text mb-25'><span
+                                        className="fw-bold text-black">Created At: </span> {tableDataDateTimeConverter(ticketDetails?.created_at)}
+                                    </p>
+
+                                    <p className='card-text mb-0'><span
+                                        className="fw-bold text-black">Status: </span> <Badge
+                                        color={ticketDetails?.status === "closed" ? 'danger' : ticketDetails?.status === 'open' ? 'success' : 'secondary'}>{ticketDetails?.status}</Badge>
+                                    </p>
+
+
+                                    <Button color='primary' className="mt-2 d-flex align-self-center" outline
+                                            disabled={ticketDetails.status !== 'open'}
+                                            onClick={() => changeStatusOnTicket()}>
+                                        {ticketDetails?.status === 'open' ? "Resolve" : "Resolved"}
+                                    </Button>
+                                </Col>
+
+                                {productItems.map((item, index) => (
+                                    <Col lg={12} className="mt-2 pt-2 separator" key={index}>
+                                        {item.contribution_product !== null ? (
+                                            <>
+                                                <h4 className="text-md font-bold">Product Details</h4>
+                                                <p className='card-text mb-25'><span
+                                                    className="fw-bold text-black">Name: </span>{item.contribution_product.name}
+                                                </p>
+                                                <p className='card-text mb-25 three-lines'>
+                          <span
+                              className="fw-bold text-black">Description: </span>
+                                                    {item.contribution_product.description}
+                                                </p>
+
+                                                <p className='card-text mb-25 three-lines'>
+                          <span
+                              className="fw-bold text-black">Package: </span>
+                                                    {item.package.name}
+                                                </p>
+
+                                                <p className='card-text mb-25 three-lines'>
+                          <span
+                              className="fw-bold text-black">Subscription: </span>
+                                                    {item.subscription.name}
+                                                </p>
+
+                                                <p className='card-text mb-0'>
+                          <span
+                              className="fw-bold text-black">Price: </span> {setCurrencyWithSymbol(item.package.price)}
+                                                </p>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <h4 className="text-md font-bold">Product Details</h4>
+                                                <p className='card-text mb-25'><span
+                                                    className="fw-bold text-black">Name: </span>{item.bulk_product.name}
+                                                </p>
+                                                <p className='card-text mb-25 three-lines'>
+                          <span
+                              className="fw-bold text-black">Description: </span>
+                                                    {item.bulk_product.description}
+                                                </p>
+
+                                                <p className='card-text mb-0'>
+                          <span
+                              className="fw-bold text-black">Price: </span> {setCurrencyWithSymbol(item.package.price)}
+                                                </p>
+                                            </>
+                                        )}
+                                    </Col>
+                                ))}
+                            </Row>
+
+                        </div>
+                    </div>
+
+                    <div className='content-wrapper'>
+
+
+                        <div className='content-body'>
+                            <div
+                                className={classnames('body-content-overlay', {
+                                    show: userSidebarRight === true || sidebar === true || userSidebarLeft === true
+                                })}
+                                onClick={handleOverlayClick}
+                            ></div>
+                            <Chat
+                                chatDetails={chatList}
+                                handleUser={handleUser}
+                                handleSidebar={handleSidebar}
+                                userSidebarLeft={userSidebarLeft}
+                                handleUserSidebarRight={handleUserSidebarRight}
+                                replyCallback={(e) => {
+                                    replyHandler(e)
+                                }}
+                                ticketDetails={ticketDetails}
+                            />
+                        </div>
+                    </div>
+
+                </PerfectScrollbar>
+
+
+            </div>
+        </Fragment>
+    )
+}
+
+export default AppChat
